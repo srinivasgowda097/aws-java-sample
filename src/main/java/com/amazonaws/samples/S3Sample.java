ca185981720476a093691212e605c2da52162988
srinivasgowda097 <srinivasgowda097@gmail.com>
2015-03-05 14:12:19 +0530

    Update S3Sample.java

--- src/main/java/com/amazonaws/samples/S3Sample.java
+++ src/main/java/com/amazonaws/samples/S3Sample.java
@@ -22,7 +22,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.util.UUID;
+import java.util.UUID; ...ktdgsjryj
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
