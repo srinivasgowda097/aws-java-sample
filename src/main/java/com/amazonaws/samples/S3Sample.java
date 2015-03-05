6e8635b9672fbd56e0be29b288f4518c9c69a29b
srinivasgowda097 <srinivasgowda097@gmail.com>
2015-03-05 14:07:40 +0530

    Update S3Sample.java

--- src/main/java/com/amazonaws/samples/S3Sample.java
+++ src/main/java/com/amazonaws/samples/S3Sample.java
@@ -12,7 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package com.amazonaws.samples;
+package com.amazonaws.samples;//commets
 
 import java.io.BufferedReader;
 import java.io.File;
